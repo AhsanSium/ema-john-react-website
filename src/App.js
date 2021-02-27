d5270bb4909c07933ef13a422e10e9980177b271
Ahsan Sium <68369475+AhsanSium@users.noreply.github.com>
2021-02-27 18:15:05 +0600

    Updating Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Shop from './components/Header/Shop/Shop';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
